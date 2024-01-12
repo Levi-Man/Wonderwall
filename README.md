19383d3d49a497955c2297bedcc101faf82b1021
Morgan <morgs99@gmail.com>
2024-01-11 23:47:21 -0500

    finished fetch requests

---
+++ README.md
@@ -0,0 +1,5 @@
+### Credits
+
+[Musicbrainz Database / API](https://musicbrainz.org/)<br>
+[CoverArt Archive](https://coverartarchive.org/)<br>
+[Billboard Top 100 NPM Library](https://www.npmjs.com/package/billboard-top-100)
--- apicheck.js
+++ apicheck.js
@@ -34,7 +34,7 @@ let check = async () => {
 
         const artistData = await fetch('http://musicbrainz.org/ws/2/artist/5b11f4ce-a62d-471e-81fc-a69a8278c7da?inc=aliases&fmt=json');
         const jsonArtist = await artistData.json();
-        console.log(jsonArtist);
+        // console.log(jsonArtist);
         const artistHtml = `<h2>${jsonArtist.name}</h2>`;
         const artist = document.getElementById("artist");
         artist.innerHTML = artistHtml;
@@ -43,33 +43,42 @@ let check = async () => {
 
         const songData = await fetch('https://musicbrainz.org/ws/2/release-group/e849bcf2-c581-3b27-98be-a1292754f234?inc=aliases%2Bartist-credits%2Breleases&fmt=json');
         const jsonSong = await songData.json();
-        console.log(jsonSong);
+        // console.log(jsonSong);
         const songHtml = `<h2>${jsonSong.title}</h2>`;
         const song = document.getElementById("song");
         song.innerHTML = songHtml;
 
         // Genre lookup "Alternative"
 
-        //     const genreData = await fetch('http://musicbrainz.org/ws/2/genre/???&fmt=json');
-        //     const jsonGenre = await genreData.json();
-        //     console.log(jsonGenre);
-        //     const genreHtml = `<h2>${jsonGenre...
-        // }</h2 > `;
-        //     const genre = document.getElementById("genre");
-        //     genre.innerHTML = genreHtml;
+        const genreData = await fetch('http://musicbrainz.org/ws/2/tag/?query=rock&fmt=json');
+        const jsonGenre = await genreData.json();
+        // console.log(jsonGenre);
+        const genreHtml = `<h2><a href="https://musicbrainz.org/genre/${jsonGenre.tags[0].name}">${jsonGenre.tags[0].name}</a></h2> `;
+        const genre = document.getElementById("genre");
+        genre.innerHTML = genreHtml;
 
         // Year lookup "1994"
 
-        // const yearData = await fetch('http://musicbrainz.org/ws/2/release/***?inc=artist-credits+labels+discids+recordings&fmt=json');
-        // const yearHtml = `< div ><h2>${}</h2><p>${}</p></div > `;
+        const yearData = await fetch('http://musicbrainz.org/ws/2/release/?query=date:1994-02-01&fmt=json');
+        const jsonYear = await yearData.json();
+        // console.log(jsonYear);
+        const yearHtml = `<h2>${jsonYear.releases[2].date}</h2> `;
+        const year = document.getElementById("year");
+        year.innerHTML = yearHtml;
 
-        // const jsonD = await data.json();
+        // const albumData = await fetch('http://musicbrainz.org/ws/2/release-group/?query=alias:queen&primarytype:album&release:queen&fmt=json');
+        // const jsonAlbum = await albumData.json();
+        // console.log(jsonAlbum);
+        // const albumHtml = `<h2>${jsonAlbum.release - groups[0].title}</h2>`;
+        // const album = document.getElementById("album");
+        // album.innerHTML = albumHtml;
 
-        // console.log(jsonD);
-
-
-        // console.log(jsonD.name);
-
+        const cover = await fetch('https://coverartarchive.org/release/4119c891-2706-3748-ae10-44976b191f2d');
+        const jsonCover = await cover.json();
+        // console.log(jsonCover);
+        const coverHtml = `<img src="${jsonCover.images[0].image}">`;
+        const coverspot = document.getElementById("cover");
+        coverspot.innerHTML = coverHtml;
     }
     catch (err) {
         console.log(err);
