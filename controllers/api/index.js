7aca4ad101cff58e4f61d731bf51887f26e02e36
TammyH7 <tamara.t.harry@gmail.com>
2024-01-13 20:24:44 -0500

    updated added user model and updated user-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 // const userRoutes = require('./userRoutes');
 const artistRoutes = require('./artistRoutes');
+const userRoutes = require('./userRoutes');
 
 // router.use('/users', userRoutes);
 router.use('/artist', artistRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
