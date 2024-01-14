35c9601d9b8c0caf740c96dc4738db790bbb37e9
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2024-01-13 23:15:33 -0500

    added albumRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 // const userRoutes = require('./userRoutes');
 const artistRoutes = require('./artistRoutes');
 
+const albumRoutes = require('./albumRoutes');
+
+router.use('/album', albumRoutes);
 // router.use('/users', userRoutes);
 router.use('/artist', artistRoutes);
 
