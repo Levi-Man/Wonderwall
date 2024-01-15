4f62e6bffdba14c57aeb4f50143a930b73875689
Morgan <morgs99@gmail.com>
2024-01-14 23:16:28 -0500

    bcs help with chart partial

---
+++ public/js/chart-script.js
@@ -0,0 +1,16 @@
+const { getChart } = require('billboard-top-100');
+
+getChart('hot-100', '1994-01-01', (err, chart) => {
+    if (err) console.log(err);
+    for (let i = 0; i < 10; i++) {
+        // console.log(chart.songs[i].title);
+        // console.log(chart.songs[i].artist);
+        // console.log(chart.songs[i].cover);
+        let charter = document.getElementById(`"chart-${i}"`);
+        let chartItemHtml =
+            `<p>${chart.songs[i].title}></p>
+        <p>${chart.songs[i].artist}</p>
+        <img src="${chart.songs[i].cover}" alt="${chart.songs[i].title}" height="35" width="35">`;
+        charter.innerHTML = chartItemHtml;
+    }
+});
