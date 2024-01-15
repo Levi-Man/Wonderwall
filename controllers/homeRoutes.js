c2440e5b6065dac7f45f3af660d82e5b9c65cc6e
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2024-01-15 11:49:33 -0500

    added search.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,19 +27,34 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+
 router.get('/artist/:id', async (req, res) => {
   try {
-    const artistData = await Artist.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+    const artistData = await fetch(`http://musicbrainz.org/ws/2/artist/?query=${req.params.id}&method=indexed&inc=aliases&fmt=json`, {
+      method: 'GET',
+      headers: {
+        'user-agent': 'Wonderwall/<1.0> ( morgs99@gmail.com )'
+      }
     });
-
-    const artist = artistData.get({ plain: true });
-
+    // console.log(artistData);
+    const jsonArtist = await artistData.json();
+    const artistName = jsonArtist["artists"][0].name;
+    const releasesData = await fetch(`http://musicbrainz.org/ws/2/release/?query=arid:${jsonArtist["artists"][0].id}&primarytype=Album&fmt=json`, {
+      method: 'GET',
+      headers: {
+        'user-agent': 'Wonderwall/<1.0> ( morgs99@gmail.com )'
+      }
+    });
+    const jsonReleases = await releasesData.json();
+    const albums = jsonReleases.releases.map(release => ({
+      title: release.title,
+      releaseDate: release.date,
+    }));
+    const responseData = {
+      artistName,
+      albums,
+    };
     res.render('artist', {
       ...artist,
       logged_in: req.session.logged_in
@@ -80,3 +95,47 @@ router.get('/login', (req, res) => {
 });
 
 module.exports = router;
+// const router = require('express').Router();
+// const { Artist, Genre, Song, Year, Album } = require('../models');
+// // const withAuth = require('../utils/auth');
+// router.get('/', async (req, res) => {
+//     console.log('connected');
+// });
+// router.get('/artist/:id', async (req, res) => {
+//     try {
+//         const artistData = await fetch(`http://musicbrainz.org/ws/2/artist/?query=${req.params.id}&method=indexed&inc=aliases&fmt=json`, {
+//             method: 'GET',
+//             headers: {
+//                 'user-agent': 'Wonderwall/<1.0> ( morgs99@gmail.com )'
+//             }
+//         });
+//         // console.log(artistData);
+//         const jsonArtist = await artistData.json();
+//         const artistName = jsonArtist["artists"][0].name;
+//         const releasesData = await fetch(`http://musicbrainz.org/ws/2/release/?query=arid:${jsonArtist["artists"][0].id}&primarytype=Album&fmt=json`, {
+//             method: 'GET',
+//             headers: {
+//                 'user-agent': 'Wonderwall/<1.0> ( morgs99@gmail.com )'
+//             }
+//         });
+//         const jsonReleases = await releasesData.json();
+//         const albums = jsonReleases.releases.map(release => ({
+//             title: release.title,
+//             releaseDate: release.date,
+//         }));
+//         const responseData = {
+//             artistName,
+//             albums,
+//         };
+//         // console.log(jsonArtist);
+//         res.status(200).json(responseData);
+//         // res.render('artist', {
+//         //     jsonArtist,
+//         //     logged_in: req.session.logged_in
+//         // });
+//     } catch (err) {
+//         console.error(err)
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// });
+// module.exports = router;
