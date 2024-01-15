f28d9361e77c27982c16cc574faedefdc9049f31
Morgan <morgs99@gmail.com>
2024-01-14 23:49:53 -0500

    small changes to check

--- public/js/chart-script.js
+++ public/js/chart-script.js
@@ -6,11 +6,13 @@ getChart('hot-100', '1994-01-01', (err, chart) => {
         // console.log(chart.songs[i].title);
         // console.log(chart.songs[i].artist);
         // console.log(chart.songs[i].cover);
-        let charter = document.getElementById(`"chart-${i}"`);
+        let charter = document.getElementById("chart-list");
         let chartItemHtml =
-            `<p>${chart.songs[i].title}></p>
-        <p>${chart.songs[i].artist}</p>
-        <img src="${chart.songs[i].cover}" alt="${chart.songs[i].title}" height="35" width="35">`;
-        charter.innerHTML = chartItemHtml;
+            `<li class="chart-item" id="chart-${i}>
+            <p>${chart.songs[i].title}></p>
+            <p>${chart.songs[i].artist}</p>
+            <img src="${chart.songs[i].cover}" alt="${chart.songs[i].title}" height="35" width="35">
+            </li>`;
+        charter.append(chartItemHtml);
     }
 });
