9c6c82f15ad1ed40be9afbedf8cf8861916ce275
Morgan <morgs99@gmail.com>
2024-01-15 13:13:15 -0500

    year small fixes

--- controllers/api/yearRoutes.js
+++ controllers/api/yearRoutes.js
@@ -1,22 +1,15 @@
 const router = require('express').Router();
 const { getChart } = require('billboard-top-100');
-// const { Year } = require('../../models');
 
 
 router.get('/:id', async (req, res) => {
     try {
-        // console.log(chart.songs[i].title);
-        // console.log(chart.songs[i].artist);
-        // console.log(chart.songs[i].cover);
 
         await getChart('hot-100', `${req.params.id}-01-01`, (err, chart) => {
             if (err) console.log(err);
 
-            // console.log(chart.songs[0].title);
-
             const filteredChart = chart.songs.filter((c) => c.rank < 11);
 
-            // console.log(filteredChart);
             const printYear = (req.params.id);
 
             res.render('partials/chart-details', {
@@ -25,8 +18,6 @@ router.get('/:id', async (req, res) => {
 
         }
         )
-        // console.log(chartData);
-
     } catch (err) {
         res.status(500).json(err);
     }
